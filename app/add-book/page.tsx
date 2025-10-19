b186ca48dc5f03eb9a170d7e08b5a35673650aac
Kamil Wojtol <kamil.wojtol97@gmail.com>
2025-10-19 10:46:15 +0200

    minor styling changes

--- app/add-book/page.tsx
+++ app/add-book/page.tsx
@@ -2,7 +2,7 @@ import { Input, Select } from "@headlessui/react";
 
 export default function AddBook() {
   return (
-    <div className="flex justify-center flex-col ">
+    <div className="flex justify-center flex-col">
       <h2 className="mt-3 ml-2">Add Book</h2>
       <form className="mx-auto w-1/3">
         <label
@@ -12,7 +12,7 @@ export default function AddBook() {
           Add Image
         </label>
         <input
-          className="file:mt-3 file:h-20 file:w-full file:rounded-lg file:border-none file:bg-white/5 px-3 py-1.5 file:text-sm/6 text-whiteblock w-full text-sm text-gray-500"
+          className="file:mt-3 file:mr-2 file:h-20 file:w-1/3 file:rounded-lg file:border-none file:bg-white/5 hover:file:bg-white/7 focus:bg-white/7 py-2 file:text-sm/6 text-whiteblock w-full text-sm text-gray-500 cursor-pointer"
           type="file"
           id="image"
           name="image"
@@ -20,7 +20,7 @@ export default function AddBook() {
         <label htmlFor="title">
           <h3 className="mt-2 text-white font-bold">Title</h3>
           <Input
-            className="mt-3 block w-full rounded-lg border-none bg-white/5 px-3 py-1.5 text-sm/6 text-white"
+            className="mt-3 h-10 block w-full rounded-lg border-none bg-white/5 hover:bg-white/7 focus:bg-white/7 px-3 py-1.5 text-sm/6 text-white outline-0"
             type="text"
             id="title"
             name="title"
@@ -29,7 +29,7 @@ export default function AddBook() {
         <label htmlFor="author">
           <h3 className="mt-2 text-white font-bold">Author</h3>
           <Input
-            className="mt-3 block w-full rounded-lg border-none bg-white/5 px-3 py-1.5 text-sm/6 text-white"
+            className="mt-3 h-10 block w-full rounded-lg border-none bg-white/5 hover:bg-white/7 focus:bg-white/7 px-3 py-1.5 text-sm/6 text-white outline-0"
             type="text"
             id="author"
             name="author"
@@ -38,17 +38,17 @@ export default function AddBook() {
         <label htmlFor="genre">
           <h3 className="mt-2 text-white font-bold">Genre</h3>
           <Select
-            className="mt-3 block w-full rounded-lg border-none bg-white/5 px-3 py-1.5 text-sm/6 text-white"
+            className="mt-3 h-10 block w-full rounded-lg border-none bg-white/5 hover:bg-white/7 px-3 py-2 text-sm/6 text-white outline-0"
             id="genre"
             name="genre"
           >
-            <option className="mt-3 block w-full rounded-lg border-none bg-white/5 px-3 py-1.5 text-sm/6 text-white">
+            <option className="mt-3 block w-full rounded-lg border-none bg-white/5 hover:bg-white/7 focus:bg-white/7 px-3 py-1.5 text-sm/6 text-white">
               Fantasy
             </option>
-            <option className="mt-3 block w-full rounded-lg border-none bg-white/5 px-3 py-1.5 text-sm/6 text-white">
+            <option className="mt-3 block w-full rounded-lg border-none bg-white/5 hover:bg-white/7 focus:bg-white/7 px-3 py-1.5 text-sm/6 text-white">
               Crime
             </option>
-            <option className="mt-3 block w-full rounded-lg border-none bg-white/5 px-3 py-1.5 text-sm/6 text-white">
+            <option className="mt-3 block w-full rounded-lg border-none bg-white/5 hover:bg-white/7 focus:bg-white/7 px-3 py-1.5 text-sm/6 text-white">
               Novel
             </option>
           </Select>
